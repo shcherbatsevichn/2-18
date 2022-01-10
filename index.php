573ff9c5bdedd05339dec32247f4746890d40ae1
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-10 17:09:42 +0300

    2-18

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php error_reporting(-1);
+//В массиве А(N) подсчитать количество различных элементов.  
+$A = [1, -7, -4, 2, -7, 2, 3, 3, -199, 4, 1, 4, -20, 6, 6, 3, -4, 13, 99];
+
+
+echo("A(n):<br>");
+print_r($A);
+echo("<br>Result:<br>");
+$a = count_diff_elem($A);
+echo("В массиве А(N) {$a} различных элементов");
+
+function count_diff_elem($array){
+    $countres = 0;
+    sort($array);
+    for($i = 0; $i < count($array); $i++){
+        if($array[$i] != $array[$i+1]){
+            $countres++;
+        }
+    }
+    return $countres;
+}        
+
